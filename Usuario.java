43a22a1f97042f85e8a6218515cdd2bf704183cb
Anggelie Lizeth Velásquez Asencio <98540747+Anggelie@users.noreply.github.com>
2022-10-05 23:06:03 -0600

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,16 @@
+    /*
+    Universidad del Valle de Guatemala
+    Programación orientada a objetos
+    Anggelie Velásquez 221181
+    */
+public class usuario{
+    public static void main(String[] args){
+      String nombre = " ";
+      String apellido1 = " ";
+      String apellido2 = " ";
+   
+      String usuario = (nombre.charAt(0)+apellido1+apellido2.charAt(0)).toLowerCase();
+      System.out.println(usuario);
+    }
+
+  }
